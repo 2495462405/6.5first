3fde367df8ff46354d01831736a04432851dff77
xuyishen <xzj2495462405@outlook.com>
2020-06-05 14:58:59 +0800

    2020.6.5mission1

--- test/test/main.cpp
+++ test/test/main.cpp
@@ -11,49 +11,47 @@
 using namespace cv;
 using namespace std;
 
-int bgSub_demo()
-{
-    VideoCapture capVideo(0);
-    if (!capVideo.isOpened()) {
-        std::cout << "Unable to open video!" << std::endl;
-        return -1;
-    }
-
-    int cnt = 0;
-    Mat frame;
-    Mat bgMat;
-    Mat subMat;
-    Mat bny_subMat;
-    Mat bgModelMat;
-
-    while (1) {
-        capVideo >> frame;
-        cvtColor(frame, frame, COLOR_BGR2GRAY);
-        if (cnt == 0) {
-            frame.copyTo(bgMat);
-        }
-        else {
-            absdiff(frame, bgMat, subMat);
-            threshold(subMat, bny_subMat, 20, 255, THRESH_BINARY);
-            imshow("b_subMat", bny_subMat);
-            imshow("frame", frame);
-            waitKey(30);
-        }
-        cnt++;
-
-    }
-    return 0;
-}
-
-
-
+int createMaskByKmeans(cv::Mat src, cv::Mat &mask);
 int main()
 {
+    Mat src = imread("/Users/xuyishen/Desktop/sa jiganyouhaximeiyouga/2020.3.27/blur/sd.jpg");
+    Mat mask = Mat::zeros(src.size(), CV_8UC1);
+    createMaskByKmeans(src, mask);
+    imshow("src", src);
+    imshow("mask", mask);
+    waitKey(0);
+}
 
-    double start = static_cast<double>(getTickCount());
-    bgSub_demo();
-    double time = ((double)getTickCount() - start) / getTickFrequency();
-    cout << "processing time:" << time / 1000 << "ms" << endl;
-    system("pause");
+int createMaskByKmeans(cv::Mat src, cv::Mat & mask)
+{
+    if ((mask.type() != CV_8UC1)
+        || (src.size() != mask.size())
+        ) {
+        return 0;
+    }
+    int width = src.cols;
+    int height = src.rows;
+
+    int pixNum = width * height;
+    int clusterCount = 2;
+    Mat labels;
+    Mat centers;
+
+    //制作kmeans用的数据
+    Mat sampleData = src.reshape(3, pixNum);
+    Mat km_data;
+    sampleData.convertTo(km_data, CV_32F);
+
+    //执行kmeans
+    TermCriteria criteria = TermCriteria(TermCriteria::EPS + TermCriteria::COUNT, 10, 0.1);
+    kmeans(km_data, clusterCount, labels, criteria, clusterCount, KMEANS_PP_CENTERS, centers);
+
+    //制作mask
+    uchar fg[2] = { 0,255 };
+    for (int row = 0; row < height; row++) {
+        for (int col = 0; col < width; col++) {
+            mask.at<uchar>(row, col) = fg[labels.at<int>(row*width + col)];
+        }
+    }
     return 0;
 }
